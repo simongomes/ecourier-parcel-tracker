48ec6aa7d343af7ba33445e2b6351dfaaa609139
Simon Gomes <busy.s.simon@gmail.com>
2021-04-11 02:00:53 +0600

    Admin Menu Registered

--- ecourier-parcel-tracker.php
+++ ecourier-parcel-tracker.php
@@ -25,6 +25,8 @@ require_once __DIR__ . '/vendor/autoload.php';
  * Class Ecourier_Parcel_Tracker.
  */
 final class Ecourier_Parcel_Tracker {
+
+	const VERSION = '1.0.1';
 	/**
 	 * Ecourier_Parcel_Tracker constructor.
 	 */
@@ -57,7 +59,7 @@ final class Ecourier_Parcel_Tracker {
 	 * @return void
 	 */
 	public function define_constants() {
-		define( 'EPT_VERSION', '1.0.0' );
+		define( 'EPT_VERSION', self::VERSION );
 		define( 'EPT_FILE', __FILE__ );
 		define( 'EPT_PATH', __DIR__ );
 		define( 'EPT_URL', plugins_url( '', EPT_FILE ) );
--- includes/Admin/class-menu.php
+++ includes/Admin/class-menu.php
@@ -12,4 +12,22 @@ namespace SimonGomes\EPT\Admin;
  */
 class Menu {
 
+	/**
+	 * Menu constructor.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+	}
+
+	/**
+	 * Register the admin menu for the EPT plugin.
+	 *
+	 * @return void
+	 */
+	public function admin_menu() {
+		add_menu_page( __( 'eCourier Tracker', 'ecourier-parcel-tracker' ), __( 'eCourier Tracker', 'ecourier-parcel-tracker' ), 'manage_options', 'ecourier-parcel-tracker', array( $this, 'plugin_page' ), 'dashicons-cart' );
+	}
+
 }
